12e25dfbf7f54d5085fa9adc5e5be8b856796fe1
sanjaraybekov <sanjaraliaybekov@gmail.com>
2022-02-07 02:07:47 +0500

    okay

--- bot.js
+++ bot.js
@@ -3,10 +3,10 @@ const {Telegraf}=require("telegraf");
 
 const bot =new Telegraf("5259679287:AAELWsIupsDZOOP7XVDeCpidEDUO0NhGvcc");
 
-bot.start((ctx)=>ctx.reply("Welcome!"));
+bot.start((ctx)=>ctx.reply("Hi! Welcome!"));
 
 bot.help((ctx)=>ctx.reply("What?"));
 
-bot.settings((ctx)=>ctx.reply("You entered settings."))
+bot.settings((ctx)=>ctx.reply("You entered settings."));
 
 bot.launch();
